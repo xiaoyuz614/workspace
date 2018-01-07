da251f4147a0c1fdd9e47f5fec60c686d16072b4
xiaoyuz614 <xiaoyuz.study@gmail.com>
2018-01-07 17:07:55 -0500

    fixed xml problem

--- src/main/java/demo/rest/RunningBulkUploadRestController.java
+++ src/main/java/demo/rest/RunningBulkUploadRestController.java
@@ -1,14 +1,42 @@
 package demo.rest;
 
 
-import demo.Service.impl.LocationService;
+import demo.service.LocationService;
 import demo.domain.Location;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RunningBulkUploadRestController {
 
     @Autowired
     private LocationService locationService;
+
+    @RequestMapping(value="/running", method= RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+
+
+    public void upload(@RequestBody List<Location> locations){
+        locationService.saveRunningLocations(locations);
+    }
+
+    @RequestMapping(value="/purge", method= RequestMethod.DELETE)
+    public void purge(){
+        this.locationService.deleteAll();
+    }
+    @RequestMapping(value="/running/{movementType}",method = RequestMethod.GET)
+    public Page<Location> findByMovementType(@PathVariable String movementType, @RequestParam(name="page") int page, @RequestParam(name="size") int size){
+          return this.locationService.findByRunnerMovementType(movementType,new PageRequest(page,size));
+    }
+
+    @RequestMapping(value = "/running/runningId/{runningId}",method = RequestMethod.GET)
+    public Page<Location> findByRunningId(@PathVariable String runningId,@RequestParam(name="page") int page, @RequestParam(name="size") int size){
+        return this.locationService.findByRunningId(runningId,new PageRequest(page,size));
+    }
+
 }
